bedb4f968baf0805e0b8fd16e2061df9d096d116
juny9797 <ksjnb6065@gmail.com>
2025-07-30 17:43:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,35 +14,35 @@ nest_asyncio.apply()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # =============================
-# 📌 메시지 내용
+# 📌 메시지 내용 (채널보기만 하이퍼링크)
 # =============================
 MESSAGE = """
 💎<b>제휴 리스트</b>💎
 
-▫️<a href="http://t.me/ocn2Marketing">구인/구직 채널보기</a>
-▫️<a href="http://t.me/ocn9Marketing">문자발송 업체 채널보기</a>
-▫️<a href="http://t.me/ocn11Marketing">토토/카지노/홀덤 채널보기</a>
+▫️구인/구직 <a href="http://t.me/ocn2Marketing">채널보기</a>
+▫️문자발송 업체 <a href="http://t.me/ocn9Marketing">채널보기</a>
+▫️토토/카지노/홀덤 <a href="http://t.me/ocn11Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn10Marketing">각종DB 업자 채널보기</a>
-▫️<a href="http://t.me/ocn1Marketing">DDOS/보안 관련 채널보기</a>
-▫️<a href="http://t.me/ocn6Marketing">웹개발/솔루션/제작 채널보기</a>
+▫️각종DB 업자 <a href="http://t.me/ocn10Marketing">채널보기</a>
+▫️DDOS/보안 관련 <a href="http://t.me/ocn1Marketing">채널보기</a>
+▫️웹개발/솔루션/제작 <a href="http://t.me/ocn6Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn12Marketing">디자인/이미지/영상 채널보기</a>
+▫️디자인/이미지/영상 <a href="http://t.me/ocn12Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn14Marketing">종합 마케팅/홍보관련 채널보기</a>
-▫️<a href="http://t.me/ocn3Marketing">유입/퍼미션/콜센터 채널보기</a>
-▫️<a href="http://t.me/ocn4Marketing">010인증/계정관련 채널보기</a>
-▫️<a href="http://t.me/ocn5Marketing">유심/라우터/서버 채널보기</a>
+▫️종합 마케팅/홍보관련 <a href="http://t.me/ocn14Marketing">채널보기</a>
+▫️유입/퍼미션/콜센터 <a href="http://t.me/ocn3Marketing">채널보기</a>
+▫️010인증/계정관련 <a href="http://t.me/ocn4Marketing">채널보기</a>
+▫️유심/라우터/서버 <a href="http://t.me/ocn5Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn8Marketing">세탁/가상/PG 채널보기</a>
-▫️<a href="http://t.me/ocn7Marketing">코인대행/OTC 관련 채널보기</a>
+▫️세탁/가상/PG <a href="http://t.me/ocn8Marketing">채널보기</a>
+▫️코인대행/OTC 관련 <a href="http://t.me/ocn7Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn19Marketing">은행업무/장관련 채널보기</a>
-▫️<a href="http://t.me/ocn15Marketing">깡/소액/대리결제 채널보기</a>
+▫️은행업무/장관련 <a href="http://t.me/ocn19Marketing">채널보기</a>
+▫️깡/소액/대리결제 <a href="http://t.me/ocn15Marketing">채널보기</a>
 
-▫️<a href="http://t.me/ocn27Marketing">유흥업소 수도권 채널보기</a>
-▫️<a href="http://t.me/ocn13Marketing">명품/레플리카/주얼리 채널보기</a>
-▫️<a href="http://t.me/ocn26Marketing">TV/영화/애니/중계 채널보기</a>
+▫️유흥업소 수도권 <a href="http://t.me/ocn27Marketing">채널보기</a>
+▫️명품/레플리카/주얼리 <a href="http://t.me/ocn13Marketing">채널보기</a>
+▫️TV/영화/애니/중계 <a href="http://t.me/ocn26Marketing">채널보기</a>
 
 📢 <b>공지 이벤트</b> : @KBOVVIP365
 
